234fe583005d739546ff47acd9cbd92873380a84
Aeron <ajlaffere@gmail.com>
2024-06-24 02:14:04 +0100

    initial work

--- backend/vouch_app/models.py
+++ backend/vouch_app/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Paper(models.Model):
+    title = models.CharField(max_length=200)
+    url = models.URLField()
+    votes = models.IntegerField(default=0)
